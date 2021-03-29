acc03218dc7660406c38700415bcdebbcf551d22
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-03-29 14:30:56 +0100

    Creating REST API to Save Employee Object

--- springboot-backend/src/main/java/com/example/demo/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Employee;
 import com.example.demo.repository.EmployeeRepository;
 
+//add CrossOrigin to give access to the frontEnd
 @CrossOrigin(origins = "http://localhost:4200") 
 @RestController
 @RequestMapping("/api/v1/")
@@ -23,5 +26,12 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees(){
 		return employeeRepository.findAll();
+	}
+	
+	// Create Employees REST API
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+		
 	}
 }
