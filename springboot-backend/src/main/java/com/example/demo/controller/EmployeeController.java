3e88296e3b4a6d0bab1879ac44d03a56f353d1a8
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-03-30 20:01:20 +0100

    Creating DELETE REST API to Delete an Employee

--- springboot-backend/src/main/java/com/example/demo/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,12 +1,15 @@
 package com.example.demo.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.PostUpdate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,5 +63,16 @@ public class EmployeeController {
 		Employee updatedEmployee = employeeRepository.save(employee);
 		
 		return ResponseEntity.ok(updatedEmployee);
+	}
+	
+	// Delete Employee REST API
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("Employee doesn't exist with id : " + id));
+		employeeRepository.delete(employee);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted",Boolean.TRUE);
+		return ResponseEntity.ok(response);
 	}
 }
